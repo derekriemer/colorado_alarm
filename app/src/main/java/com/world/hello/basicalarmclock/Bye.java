28f747d5a4cbf596a919131fab81f743fc6da109
cudilara <coffeeshop51@yahoo.com>
2016-02-29 10:17:23 -0700

    There is a bug in AndroidManifest.xml. The project is made with the tutorial http://www.c-sharpcorner.com/UploadFile/e14021/simple-alarm-application-in-android-studio/

---
+++ app/src/main/java/com/world/hello/basicalarmclock/Bye.java
@@ -0,0 +1,22 @@
+/*package com.world.hello.basicalarmclock;*/
+
+/**
+ * Created by dilara on 2/28/2016.
+ */
+/*public class Bye {
+}
+*/
+package com.world.hello.basicalarmclock;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        import com.alarmclock.R;
+
+public class Bye extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bye_layout);
+    }
+}

e25a9a589fbb6e44d000f3af58fbd252aa7aa911
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-04-13 16:12:27 -0600

    init from mars weather demo app to port REST work

---
+++ app/src/main/java/com/robobrandon/simpleweather/MarsWeather.java
@@ -0,0 +1,37 @@
+package com.robobrandon.simpleweather;
+
+import android.app.Application;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MarsWeather extends Application {
+
+    public static final String TAG = MarsWeather.class.getSimpleName();
+    private RequestQueue mRequestQueue;
+    private static MarsWeather mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+
+    public static synchronized MarsWeather getInstance() {
+        return mInstance;
+    }
+    public RequestQueue getRequestQueue() {
+        return mRequestQueue;
+    }
+
+    public <T> void add(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancel() {
+        mRequestQueue.cancelAll(TAG);
+    }
+
+}

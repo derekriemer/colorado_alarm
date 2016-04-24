eb910d3b030de0865ac4ebbdbd2ceef3876c033d
cudilara <coffeeshop51@yahoo.com>
2016-04-24 13:34:04 -0600

    changed the sound files

--- app/src/main/java/com/robobrandon/simpleweather/AlarmReceiver.java
+++ app/src/main/java/com/robobrandon/simpleweather/AlarmReceiver.java
@@ -1,6 +1,6 @@
 package com.robobrandon.simpleweather;
 
-
+/*
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -33,5 +33,41 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
                 AlarmService.class.getName());
         startWakefulService(context, (intent.setComponent(comp)));
         setResultCode(Activity.RESULT_OK);
+    }
+}
+*/
+import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.util.Log;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("We are in the receiver", "Yes!");
+
+        //fetch extra string from the intent
+        String get_your_string = intent.getExtras().getString("extra");
+
+        Log.e("What is the key?", "get_your_string");
+
+        //fetch the extra longs from the intent
+        //tells the app which value the user picked from the drop down spinner
+        Integer get_weather_choice = intent.getExtras().getInt("weather_choice");
+
+        Log.e("Weather choice is ", get_weather_choice.toString());
+
+        //create an intent to this ringtone service
+        Intent service_intent = new Intent(context,RingtonePlayingService.class);
+
+        //pass the extra string from alarmreceiver to the ringtoneplaying service
+        service_intent.putExtra("extra", get_your_string);
+        //pass the extra integer from the receiver to the ringtone playing service
+        service_intent.putExtra("weather_choice", get_weather_choice);
+
+        //start the ringtone service
+        context.startService(service_intent);
     }
 }
